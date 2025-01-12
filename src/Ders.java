81b970059968873cee4fdb0496e10cfe092a7270
rehman <r.m.shamsaliyev@gmail.com>
2025-01-12 20:36:20 +0400

    java.ders

---
+++ src/Ders.java
@@ -0,0 +1,12 @@
+public class Ders {
+    public static void main (String[]args){
+        System.out.println("|  Hello world  |");
+        System.out.println("|               |");
+        for(int i=0;i<10;i++){
+            System.out.println(i+"|  i*i="+(i+i)+"      g    |");
+
+        }
+        System.out.println("|---------------|");
+
+    }
+}
